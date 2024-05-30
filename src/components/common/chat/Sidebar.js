793917d182c4aa605f0e237495a34a31b3a93e31
KhoaMinhmachinelearning <141332177+KhoaMinhmachinelearning@users.noreply.github.com>
2024-05-30 20:31:42 +0700

    fine

--- src/components/common/chat/Sidebar.js
+++ src/components/common/chat/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { BiFilter, BiX, BiSearch, BiHomeAlt, BiMessageRounded, BiLogOut, BiChevronDown } from 'react-icons/bi'; // Import các biểu tượng từ thư viện react-icons
-
+import { BiFilter, BiSearch, BiHomeAlt, BiMessageRounded, BiLogOut, BiChevronDown, BiBookBookmark, BiHistory, BiBookmarkHeart } from 'react-icons/bi'; 
+import { BiSolidBot } from "react-icons/bi";
 const Sidebar = () => {
   const [submenuOpen, setSubmenuOpen] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(true);
@@ -17,9 +17,8 @@ const Sidebar = () => {
     <aside className={`bg-gray-900 w-64 min-h-screen p-4 space-y-6 ${sidebarOpen ? '' : 'hidden'}`}>
       <div className="text-gray-100 text-xl">
         <div className="p-2.5 mt-1 flex items-center">
-          <BiFilter className="text-white text-4xl cursor-pointer" onClick={toggleSidebar} />
-          <h1 className="font-bold text-gray-200 text-[15px] ml-3">TailwindCSS</h1>
-          <BiX className="text-white cursor-pointer ml-28 lg:hidden" onClick={toggleSidebar} />
+          <BiSolidBot className="text-white text-4xl cursor-pointer" />
+          <h1 className="font-bold text-gray-200 text-[15px] ml-3">LearnPage</h1>
         </div>
         <div className="my-2 bg-gray-600 h-[1px]"></div>
       </div>
@@ -36,8 +35,8 @@ const Sidebar = () => {
         <span className="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
       </div>
       <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-        <BiMessageRounded />
-        <span className="text-[15px] ml-4 text-gray-200 font-bold">Messages</span>
+        <BiBookBookmark />
+        <span className="text-[15px] ml-4 text-gray-200 font-bold">Learn</span>
       </div>
       <div className="my-4 bg-gray-600 h-[1px]"></div>
       <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white" onClick={toggleSubmenu}>
@@ -50,9 +49,8 @@ const Sidebar = () => {
         </div>
       </div>
       <div className={`text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold ${submenuOpen ? '' : 'hidden'}`} id="submenu">
-        <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">Social</h1>
-        <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">Personal</h1>
-        <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">Friends</h1>
+        <div className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1 flex items-center"><BiHistory className="mr-2" /> <span>History</span></div>
+        <div className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1 flex items-center"><BiBookmarkHeart className="mr-2" /> <span>Bookmark</span></div>
       </div>
       <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
         <BiLogOut />
